8f7e4d2c291ea84d4d21ac7c911d12bc5f87216b
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-01-29 09:27:11 -0300

    First Commit

---
+++ src/main/java/com/henrique/pablo/BoardWise/BoardWiseApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.pablo.BoardWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWiseApplication.class, args);
+	}
+
+}
