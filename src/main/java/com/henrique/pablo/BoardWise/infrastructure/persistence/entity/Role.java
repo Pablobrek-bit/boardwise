1c90c6786d3323057f07b9c232d4eef2ba70ff1d
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-01-31 10:01:25 -0300

    feat: add role and user entity

---
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/entity/Role.java
@@ -0,0 +1,38 @@
+package com.henrique.pablo.BoardWise.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
+
+    private String name;
+
+    @Column(name = "created_at")
+    @Timestamp
+    private LocalDateTime createdAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = new HashSet<>();
+
+}
---
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/entity/User.java
@@ -0,0 +1,42 @@
+package com.henrique.pablo.BoardWise.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+
+    @ManyToMany(mappedBy = "users")
+    private Set<Role> roles = new HashSet<>();
+}
