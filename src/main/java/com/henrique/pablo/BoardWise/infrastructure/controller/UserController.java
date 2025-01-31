f19a0056086a0d5cf70b8f2a47df5272f468cfb7
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-01-31 13:59:08 -0300

    feat: register new user route: create

---
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.henrique.pablo.BoardWise.infrastructure.controller;
+
+import com.henrique.pablo.BoardWise.application.dto.UserRequest;
+import com.henrique.pablo.BoardWise.application.dto.UserResponse;
+import com.henrique.pablo.BoardWise.application.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<UserResponse> createUser(
+            @Valid @RequestBody UserRequest request,
+            UriComponentsBuilder uriBuilder
+    ) {
+        UserResponse response = userService.createUser(request);
+        URI location = uriBuilder.path("/users/{id}").buildAndExpand(response.id()).toUri();
+        return ResponseEntity.created(location).body(response);
+    }
+}
