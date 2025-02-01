545273b339da6c0e0b9563086774744f6f403f01
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-02-01 08:48:59 -0300

    feat: create a converter type of the user

---
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/converter/UserConverter.java
@@ -0,0 +1,66 @@
+package com.henrique.pablo.BoardWise.infrastructure.persistence.converter;
+
+import com.henrique.pablo.BoardWise.application.dto.UserRequest;
+import com.henrique.pablo.BoardWise.domain.model.RoleModel;
+import com.henrique.pablo.BoardWise.domain.model.UserModel;
+import com.henrique.pablo.BoardWise.infrastructure.persistence.entity.Role;
+import com.henrique.pablo.BoardWise.infrastructure.persistence.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserConverter {
+
+    public static UserModel toDomain(User entity) {
+        return UserModel.builder()
+                .id(entity.getId())
+                .username(entity.getUsername())
+                .passwordHash(entity.getPasswordHash())
+                .email(entity.getEmail())
+                .createdAt(entity.getCreatedAt())
+                .roles(toRoleModels(entity.getRoles()))
+                .build();
+    }
+
+    public static User toEntity(UserModel domain) {
+        return User.builder()
+                .id(domain.getId())
+                .username(domain.getUsername())
+                .passwordHash(domain.getPasswordHash())
+                .email(domain.getEmail())
+                .createdAt(domain.getCreatedAt())
+                .roles(toRoleEntitiesWithoutUsers(domain.getRoles()))
+                .build();
+    }
+
+    private static Set<RoleModel> toRoleModels(Set<Role> roles) {
+        return roles.stream()
+                .map(role -> RoleModel.builder()
+                        .id(role.getId())
+                        .name(role.getName())
+                        .createdAt(role.getCreatedAt())
+                        .build())
+                .collect(Collectors.toSet());
+    }
+
+    private static Set<Role> toRoleEntitiesWithoutUsers(Set<RoleModel> roles) {
+        return roles.stream()
+                .map(role -> Role.builder()
+                        .id(role.getId())
+                        .name(role.getName())
+                        .createdAt(role.getCreatedAt())
+                        .build())
+                .collect(Collectors.toSet());
+    }
+
+    public static User requestToEntity(UserRequest request) {
+        return User.builder()
+                .username(request.username())
+                .passwordHash(request.password())
+                .email(request.email())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+    }
+}
