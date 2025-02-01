1902e8b8ae8212269c582bfb8ee0c27f9f630890
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-02-01 08:52:22 -0300

    chore: modified the IUserRepository

--- src/main/java/com/henrique/pablo/BoardWise/domain/repository/IUserRepository.java
+++ src/main/java/com/henrique/pablo/BoardWise/domain/repository/IUserRepository.java
@@ -1,11 +1,12 @@
 package com.henrique.pablo.BoardWise.domain.repository;
 
 import com.henrique.pablo.BoardWise.domain.model.UserModel;
+import com.henrique.pablo.BoardWise.infrastructure.persistence.entity.User;
 
 import java.util.Optional;
 
 public interface IUserRepository{
-    UserModel save(UserModel user);
+    User save(User user);
     Optional<UserModel> findById(String id);
     Optional<UserModel> findByEmail(String email);
 }
