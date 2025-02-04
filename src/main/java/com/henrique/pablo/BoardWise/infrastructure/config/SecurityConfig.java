863a85b9193fc6bf540a21320825265c51e61a8f
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-02-04 19:52:33 -0300

    feat: add core config

--- src/main/java/com/henrique/pablo/BoardWise/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.henrique.pablo.BoardWise.infrastructure.config;
 
+import com.henrique.pablo.BoardWise.infrastructure.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,23 +14,32 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-//                    req.requestMatchers("/auth").permitAll();
-//                    req.requestMatchers(HttpMethod.POST, "/users").permitAll();
-//                    req.requestMatchers("/products/**").hasRole("ADMIN");
-//                    req.anyRequest().authenticated();
-                    req.anyRequest().permitAll();
+                    req.requestMatchers(HttpMethod.POST,"/auth/login").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/users").permitAll();
+                    req.anyRequest().authenticated();
                 })
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
     }
 
@@ -39,6 +51,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
