5789723877f934e9eb907eda4c28bfbd87ac6dac
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-02-06 16:44:42 -0300

    feat: add new converter method

--- src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/converter/RoleConverter.java
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/converter/RoleConverter.java
@@ -3,11 +3,18 @@ package com.henrique.pablo.BoardWise.infrastructure.persistence.converter;
 import com.henrique.pablo.BoardWise.domain.model.RoleModel;
 import com.henrique.pablo.BoardWise.infrastructure.persistence.entity.Role;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class RoleConverter {
 
+    public static Optional<RoleModel> toDomain(Optional<Role> entity) {
+        return entity.map(RoleConverter::toDomain);
+    }
+
     public static RoleModel toDomain(Role entity) {
+        if(entity == null) return null;
+
         return RoleModel.builder()
                 .id(entity.getId())
                 .name(entity.getName())
@@ -18,7 +25,13 @@ public class RoleConverter {
                 .build();
     }
 
+    public static Role toEntity(Optional<RoleModel> domain) {
+        return domain.map(RoleConverter::toEntity).orElse(null);
+    }
+
     public static Role toEntity(RoleModel domain) {
+        if(domain == null) return null;
+
         return Role.builder()
                 .id(domain.getId())
                 .name(domain.getName())
@@ -28,5 +41,6 @@ public class RoleConverter {
                         .collect(Collectors.toSet()))
                 .build();
     }
+
 
 }
