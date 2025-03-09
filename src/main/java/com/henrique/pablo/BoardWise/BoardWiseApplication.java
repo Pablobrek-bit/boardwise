a2fb542678a911b80ffa26aa5ca5434dee8e3d45
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-03-09 08:38:19 -0300

    chore: add repository and entity scan path

--- src/main/java/com/henrique/pablo/BoardWise/BoardWiseApplication.java
+++ src/main/java/com/henrique/pablo/BoardWise/BoardWiseApplication.java
@@ -2,10 +2,14 @@ package com.henrique.pablo.BoardWise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication(scanBasePackages = "com.henrique.pablo.BoardWise")
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@EnableJpaRepositories(basePackages = "com.henrique.pablo.BoardWise.infrastructure.persistence.repository")
+@EntityScan(basePackages = "com.henrique.pablo.BoardWise.infrastructure.persistence.entity")
 public class BoardWiseApplication {
 
 	public static void main(String[] args) {
