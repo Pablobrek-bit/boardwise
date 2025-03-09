7aff0a8034075112d3923a9e56333633cfa64a7f
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-03-09 08:38:47 -0300

    chore: adjust generated value strategy of the role id

--- src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/entity/Role.java
+++ src/main/java/com/henrique/pablo/BoardWise/infrastructure/persistence/entity/Role.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class Role implements GrantedAuthority {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
