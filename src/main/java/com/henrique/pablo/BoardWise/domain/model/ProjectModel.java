dee648a7e026625d18da1ac3758811fe466e246b
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-03-12 15:21:54 -0300

    refactor: isolating the business rule for the domains

--- src/main/java/com/henrique/pablo/BoardWise/domain/model/ProjectModel.java
+++ src/main/java/com/henrique/pablo/BoardWise/domain/model/ProjectModel.java
@@ -1,5 +1,6 @@
 package com.henrique.pablo.BoardWise.domain.model;
 
+import com.henrique.pablo.BoardWise.application.dto.project.ProjectRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,4 +21,13 @@ public class ProjectModel {
     private UserModel owner;
     private Set<UserModel> participants = new HashSet<>();
     private BoardListModel boardLists;
+
+    public void updateFields(ProjectRequest request){
+        if(request.name() != null){
+            this.setName(request.name());
+        }
+        if(request.description() != null){
+            this.setDescription(request.description());
+        }
+    }
 }
